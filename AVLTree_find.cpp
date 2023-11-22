e857b5f3d3ca3db8ae7cfe6ddbd48b78bc40d1c4
SuikaSei <taeyang420@gmail.com>
2023-11-22 16:49:38 +0900

    초기 커밋.

---
+++ AVLTree_find.cpp
@@ -0,0 +1,49 @@
+/*
+MIT License
+
+Copyright (c) 2023 Inha-Open-Source-Team-BBBig
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Created by ÀåÅÂ¾ç on 11/19/23.
+ */
+
+#include "AVLTree.h"
+
+int AVLTree::find(int finding_num) {
+	int depth = 0;
+	NodePointer current_node = root;
+
+	while (current_node != nullptr) {
+		if (current_node->key == finding_num) {
+			return depth;
+		}
+		else if (current_node->key > finding_num) {
+			depth += 1;
+			current_node = current_Node->left;
+		}
+		else { //current_node->key < finding_num
+			depth += 1;
+			current_node = current_node->right;
+		}
+	}
+
+	return 0; // current_node == nullptr
+}
+	
